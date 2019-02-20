37098e619ea889b46fbda974d89031d1e7af25a2
Ilya Koloskov <47630854+ilya-koloskov1@users.noreply.github.com>
2019-02-20 20:04:19 +0300

    Hw-3.1

---
+++ Dz.js
@@ -0,0 +1,18 @@
+var year = prompt('Сколько тебе лет?'); 
+var smoke = confirm('Куришь?'); 
+if (smoke==true) 
+{ if (year < 18) { 
+alert( 'Мамке расскажу' ); 
+} else { 
+alert( 'Ну и зря' ); } 
+} else { 
+if (year < 18) { 
+alert( 'Молодец и не надо' ); 
+} else { 
+alert('Так держать'); } 
+}
+
+
+
+ 
+
